ddd14ae2a6b24b77c10a78343d6c05eb9b34f292
Charles Magid <cmmagid@gmail.com>
2011-05-10 09:45:36 -0400

    Initial Commit testing emacs server with git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7f707b496b9eb88cdcd11e4b326bce185b83e7a8b5eec693ef0a2d104d071e7a961fe33a980245b94470f8762b8594148b74209610d37f81d4fe07715f14bf11'
